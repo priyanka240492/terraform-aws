5c30bb72e1e633996a43e8318929669badf5203d
priyanka240492 <priyanka.kaduluri@gmail.com>
2025-05-18 23:44:18 +0530

    MAY-18 : Deploying S3 bucket in AWS using terraform

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  description = "raw_bukcet_klp"
+  type        = string
+}
+
+variable "environment" {
+  description = "DEV"
+  type        = string
+  default     = "dev"
+}
