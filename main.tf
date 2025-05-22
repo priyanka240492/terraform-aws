6d8e20c917b3dc2d75f84fa42fee206ae7d68627
priyanka240492 <priyanka.kaduluri@gmail.com>
2025-05-23 02:37:46 +0530

    MAY-23 : Added required provider block

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # adjust version as needed
+    }
+  }
+
   backend "remote" {
     organization = "priyanka_kaduluri"
     workspaces {
@@ -6,7 +13,6 @@ terraform {
     }
   }
 }
-
 
 resource "aws_s3_bucket" "raw_bucket" {
   bucket = var.bucket_name
