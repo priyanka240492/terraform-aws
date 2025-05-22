3ab89babc443486b0da61c614be102244ecc7b00
priyanka240492 <priyanka.kaduluri@gmail.com>
2025-05-23 02:15:52 +0530

    MAY-23 : Added TFC AWS assume role

--- providers.tf
+++ providers.tf
@@ -1,3 +1,6 @@
 provider "aws" {
-  region = "us-east-1"  # Change this to your preferred region
+  region                  = "us-east-1"
+  assume_role {
+    role_arn = var.TFC_AWS_RUN_ROLE_ARN
+  }
 }
